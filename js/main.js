0a1f79cbb613fccf201c18778b8ecc3b82b4e339
marco-carangelo <marcocarangelo2016@gmail.com>
2024-05-15 09:03:19 +0200

    add: variables in main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 'use strict'
+
+// Variables definition
+
+const fizz = 'Fizz';
+console.log(fizz);
+const buzz = 'Buzz';
+console.log(buzz);
+
+const div3 = 3;
+const div5 = 5;
+console.log(div3);
+console.log(div5);
+
